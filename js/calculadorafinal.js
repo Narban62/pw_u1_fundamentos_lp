0ef7d7700516135907ce4be3a83dc37a9c2e2024
Narban62 <narban62@gmail.com>
2025-11-11 20:43:33 -0500

    Tarea 3

--- js/calculadorafinal.js
+++ js/calculadorafinal.js
@@ -1,62 +1,48 @@
-let num1 =0;
-let num2 =0;
-let display = document.getElementById('display');
+let display1 = null;
+let display2 = null;
+let operacion = null;
 
 function mostrarDisplay(valor) {
     let elemento = document.getElementById('display');
-    elemento.innerText= elemento.innerText+valor;
+    elemento.innerText = elemento.innerText + valor;
 }
 
-function operaciones(operador){
-    switch (operador) {
-        case '/':
-            display.innerText = 0;
+function limpiarDisplay() {
+    document.getElementById('display').innerText = "";
+}
+
+function operacionTipo(tipo) {
+    display1 = parseFloat(document.getElementById('display').innerText);
+    operacion = tipo;
+    limpiarDisplay();
+}
+
+function borrar() {
+    let elemento = document.getElementById('display');
+    elemento.innerText = elemento.innerText.slice(0,-1);
+}
+
+function evaluarOperacion() {
+    let display2 = parseFloat(document.getElementById('display').innerText);
+    let resultado = 0;
+    switch (operacion) {
+        case '+':
+            resultado = display1 + display2;
             break;
-    
+        case '-':
+            resultado = display1 - display2;
+            break;
+        case '*':
+            resultado = display1 * display2;
+            break;
+        case '/':
+            resultado = display1 / display2;
+            break;
+        case '%':
+            resultado = display1%display2;
+            break;
         default:
             break;
     }
-}
-
-
-function sumar(num1, num2) {
-    num1 = display.innerText;
-    num2 =  display.innerText;
-     return num1+num2;
-}
-function restar(num1, num2) {
-    num1 = display.innerText;
-    num2 =  display.innerText;
-     return num1-num2;
-}
-function multiplicar(num1, num2) {
-    num1 = display.innerText;
-    num2 =  display.innerText;
-     return num1/num2;
-}
-function dividir(num1, num2) {
-    num1 = display.innerText;
-    num2 =  display.innerText;
-     return num1num2;
-}
-function porcentaje(num1, num2) {
-    num1 = display.innerText;
-    num2 =  display.innerText;
-     return num1+num2;
-}
-
-function evaluarOperacion(tipo) { 
-    let num1 = parseFloat(document.getElementById('id_n1').value);
-    let num2 = parseFloat(document.getElementById('id_n2').value);
-    let resultado = 0;
-    if (tipo ===  '+') {
-        resultado = sumar(num1,num2);
-    }if (tipo ===  '-') {
-        resultado = restar(num1,num2);
-    }if (tipo ===  '*') {
-        resultado = multiplicar(num1,num2);
-    }if (tipo ===  '/') {
-        resultado = dividir(num1,num2);
-    }
-    document.getElementById('id_resultado').innerText = resultado;
+    document.getElementById('display').innerText = resultado;
 }
